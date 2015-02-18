70bb7252c55729799e9a63b108e4642dd7120b49
Will Kaufman <wjkhoops@gmail.com>
2015-02-18 07:24:58 -0700

    adds graphics panel, adds mouse feature

--- opencv-sandbox/src/graphics/GraphicsPanel.java
+++ opencv-sandbox/src/graphics/GraphicsPanel.java
@@ -24,38 +24,50 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 class GraphicsPanel extends JPanel implements MouseListener{
-     private static final long serialVersionUID = 1L;
-     private BufferedImage image;
-     // Create a constructor method
-     public GraphicsPanel(){
-          super();
-     }
-     /*
-      * Converts/writes a Mat into a BufferedImage.
-      * 
-      * @param matrix Mat of type CV_8UC3 or CV_8UC1
-      * @return BufferedImage of type TYPE_3BYTE_BGR or TYPE_BYTE_GRAY
-      */
-     public boolean matToBufferedImage(Mat matrix) {
-          MatOfByte mb = new MatOfByte();
-          Highgui.imencode(".jpg", matrix, mb);
-          try {
-               this.image = ImageIO.read(new ByteArrayInputStream(mb.toArray()));
-          } catch (IOException e) {
-               e.printStackTrace();
-               return false; // Error
-          }
-       return true; // Successful
-     }
-     public void paintComponent(Graphics g){
-          super.paintComponent(g);
-          if (this.image==null) return;
-        
-          g.drawImage(this.image, 0, 0, GraphicsTester.WIDTH, GraphicsTester.HEIGHT, null);
-          g.setColor(Color.WHITE);
-          g.setFont(new Font("", 0, 20));
-          g.drawString("Frame: " + GraphicsTester.FRAME, 50, 50);
-     }
+	
+    private static final long serialVersionUID = 1L;
+    private BufferedImage image;
+    private int startClickX, startClickY, endClickX, endClickY;
+     
+    // Create a constructor method
+    public GraphicsPanel(){
+         super();
+    }
+    /*
+     * Converts/writes a Mat into a BufferedImage.
+     * 
+     * @param matrix Mat of type CV_8UC3 or CV_8UC1
+     * @return BufferedImage of type TYPE_3BYTE_BGR or TYPE_BYTE_GRAY
+     */
+    public boolean matToBufferedImage(Mat matrix) {
+         MatOfByte mb = new MatOfByte();
+         Highgui.imencode(".jpg", matrix, mb);
+         try {
+              this.image = ImageIO.read(new ByteArrayInputStream(mb.toArray()));
+         } catch (IOException e) {
+              e.printStackTrace();
+              return false; // Error
+         }
+      return true; // Successful
+    }
+    public void paintComponent(Graphics g){
+         super.paintComponent(g);
+         if (this.image==null) return;
+       
+         g.drawImage(this.image, 0, 0, GraphicsTester.WIDTH, GraphicsTester.HEIGHT, null);
+         g.setColor(Color.WHITE);
+         g.setFont(new Font("", 0, 20));
+         g.drawString("Frame: " + GraphicsTester.FRAME, 50, 50);
+    }
+    
+    public void printClickArea() {
+    	System.out.println("start: " + this.startClickX + ", " + 
+    					   this.startClickY);
+    	System.out.println("end: " + this.endClickX + ", " + 
+				   this.endClickY);
+
+    }
+    
 	@Override
 	public void mouseClicked(MouseEvent event) {
 		System.out.println("Mouse clicked, location: " + event.getX() +
@@ -74,13 +86,14 @@ class GraphicsPanel extends JPanel implements MouseListener{
 	}
 	@Override
 	public void mousePressed(MouseEvent event) {
-		// TODO Auto-generated method stub
-		
+		this.startClickX = event.getX();
+		this.startClickY = event.getY();
 	}
 	@Override
 	public void mouseReleased(MouseEvent event) {
-		// TODO Auto-generated method stub
-		
+		this.endClickX = event.getX();
+		this.endClickY = event.getY();
+		this.printClickArea();
 	}
       
-}  
+}
