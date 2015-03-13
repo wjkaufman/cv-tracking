418d8c9d9f069a45fd322efcd752725e2d863d57
Will Kaufman <wjkhoops@gmail.com>
2015-03-13 15:11:09 -0600

    adds contain and overlap checking to ball class

--- opencv-sandbox/src/ballTracking/Ball.java
+++ opencv-sandbox/src/ballTracking/Ball.java
@@ -2,12 +2,60 @@ package ballTracking;
 
 public class Ball {
 	
-	private double x;
-	private double y;
+	private int x, y, width, height;
 	
 	public Ball() {
-		// TODO Auto-generated constructor stub
+		
 	}
+	
+	public Ball(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setLocation(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public boolean contains(int x, int y) {
+		boolean val = (x > this.x && x < this.x + this.width)
+					   && (y > this.y && y < this.y + this.height);
+		
+		return val;
+	}
+	
+	public boolean overlap(Ball ball2) {
+		boolean b = false;
+		b = b || this.contains(ball2.getX(), ball2.getY());
+		b = b || this.contains(ball2.getX() + ball2.getWidth(), ball2.getY());
+		b = b || this.contains(ball2.getX(), ball2.getY() + ball2.getHeight());
+		b = b || this.contains(ball2.getX() + ball2.getWidth(), ball2.getY() + ball2.getHeight());
+		
+		return b;
+	}
+	
+	
 	
 	
 
